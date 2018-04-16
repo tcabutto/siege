b3b8d0be547ae1456784e9bac0511dce95cb74a1
Tyler Cabutto <tacabutto@valdosta.edu>
2018-04-16 15:57:53 -0400

    Got script functioning and able to write to csv file for my stats. Looking to make discord bot next to import into.

--- siege_stats.py
+++ siege_stats.py
@@ -1,20 +1,47 @@
 import bs4
 import requests
-from urllib import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 my_url = 'https://r6stats.com/stats/uplay/Reload316/ranked'
+#Will use main ubi site if can bypass browser error
 
-def extract_source(url):
-     agent = {"User-Agent":"Mozilla/5.0"}
-     source=requests.get(url, headers=agent).text
-     return source
 
-def extract_data(source):
-     soup=bs4.BeautifulSoup(source, 'lxml')
-     ranked = soup.find_all('div', class_='content')
-     rank = ranked[0]
-     print rank.text
-     
 
-extract_data(extract_source(my_url))
+
+agent = {"User-Agent":"Mozilla/5.0"}
+source = requests.get(my_url, headers=agent).text
+
+page_soup = soup(source, 'lxml')
+row1 = page_soup.find('div', class_='row stats')
+row2 = page_soup.find('div', class_='row stats second')
+
+#Grabbing all the values for each above
+vals1 = row1.find_all('div', class_='value')
+vals2 = row2.find_all('div', class_='value')
+
+filename = 'r6stats.csv'
+f = open(filename, 'w')
+
+headers = "kills, deaths, K/D ratio, playtime, record, win percentage, XP until lvl up, overall level\n"
+f.write(headers)
+
+kills       = vals1[0].text.strip()
+deaths      = vals1[1].text.strip()
+kd          = vals1[2].text.strip()
+playtime    = vals1[3].text.strip()
+record      = vals2[0].text.strip()
+winPercent  = vals2[1].text.strip()
+levelUp     = vals2[2].text.strip()
+level       = vals2[3].text.strip()
+
+print('Kills: ' + kills)
+print('Deaths: ' + deaths)
+print('K/D ratio: ' + kd)
+print('Playtime: ' + playtime)
+print('record: ' + record)
+print('winPercent: ' + winPercent)
+print('xp until lvl up: ' + levelUp)
+print('Overall level: ' + level)
+
+f.write(kills + "," + deaths + "," + kd + "," + playtime + "," + record + "," + winPercent + "," + levelUp.replace(",", "") + "," + level + "\n")
+f.close()
